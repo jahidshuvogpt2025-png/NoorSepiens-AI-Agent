4146f21a9ef0c465200925dc1af728747c91914e
jahidshuvogpt2025-png <jahidshuvogpt2025@gmail.com>
2026-08-15 15:58:14 +0600

    Update router.js

--- router.js
+++ router.js
@@ -197,4 +197,106 @@ role:"system",
 
 content:
 `
-ত
+তুমি NoorSepiens AI।
+
+ওয়েব সার্চ থেকে পাওয়া তথ্য ব্যবহার করে
+উত্তর তৈরি করবে।
+
+ব্যবহারকারী realtime তথ্য চাইলে
+search result ছাড়া উত্তর দেবে না।
+
+বাংলায় উত্তর দাও।
+তথ্য সংক্ষেপে এবং পরিষ্কারভাবে দাও।
+প্রয়োজনে source উল্লেখ করো।
+`
+
+}
+
+
+},
+
+
+{
+
+headers:{
+
+Authorization:
+`Bearer ${process.env.OPENROUTER_API_KEY}`,
+
+"Content-Type":
+"application/json"
+
+}
+
+}
+
+
+);
+
+
+
+
+console.log(
+"Final AI:",
+finalResponse.data.choices[0].message
+);
+
+
+
+return finalResponse
+.data
+.choices[0]
+.message
+.content;
+
+
+
+}
+
+
+}
+
+
+
+
+// Normal AI answer
+
+
+if(aiMessage.content){
+
+return aiMessage.content;
+
+}
+
+
+
+
+return "দুঃখিত, কোনো উত্তর পাওয়া যায়নি ❌";
+
+
+
+
+
+}
+
+catch(error){
+
+
+console.log(
+"AI ERROR:",
+error.response?.data || error.message
+);
+
+
+return "AI উত্তর দিতে সমস্যা হচ্ছে ❌";
+
+
+}
+
+
+
+}
+
+
+
+module.exports = router;
